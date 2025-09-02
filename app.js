47227bade465a5899955bbfba60b53fed31dfcc7
Moh0147 <ahmadmuh0147@gmail.com>
2025-09-02 20:09:52 +0300

    HTML, CSS and litle JS

---
+++ app.js
@@ -0,0 +1,27 @@
+// let x= 2;
+// let y= 4;
+// let sum=  x + y;
+// 
+// if(sum%sum==0 && sum% 1==0){
+//     console.log("The sum is prime")
+// }
+// else{
+//     console.log("not")
+// }
+
+
+// string methods
+
+const greet = "helloFromDit"
+
+//length of a string
+console.log(greet.length)
+
+//convert string into array
+console.log(greet.split(""))
+
+//includes method
+console.log(greet.includes("mohamed"))
+
+//substring method
+console.log(greet.substring(0,4))
